4251767eeabe55678ce39363379912b0bc957d3a
Vertti Laitinen <vjj.laitinen@gmail.com>
2025-03-29 20:06:42 +0200

    Functional home page added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+// Serve static files (CSS, images, JS)
+app.use(express.static(path.join(__dirname, "public")));
+
+// Homepage route
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
